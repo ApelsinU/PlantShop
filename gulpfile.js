3f19a7dcb5781d00488b28936ed2e9858359a592
Vadim Kaptsov <vadimkaptsov@mail.ru>
2022-01-28 10:19:17 +0300

    gulp - add config to js files

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const browserSync = require('browser-sync').create();
 const Pug = require('gulp-pug');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
 
 function browsersync() {
     browserSync.init({
@@ -33,9 +34,18 @@ function scss() {
 function watch() {
   gulp.watch('./src/', pug);
   gulp.watch('./src/assets/', scss);
+  gulp.watch('./src/assets/', scripts);
+}
+
+function scripts() {
+  return gulp.src(['./src/**/*.js'])
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest(['./dist/js']))
+    .on('end', browserSync.reload)
 }
 
 exports.default = gulp.series(
   gulp.parallel(pug, scss),
-  gulp.parallel(browsersync, watch)
+  gulp.parallel(browsersync, watch),
+  gulp.parallel(scripts, watch)
 );

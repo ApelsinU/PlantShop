0522295da88f374761a75ad1085ffb5ab18e3a69
Vadim Kaptsov <vadimkaptsov@mail.ru>
2022-01-28 10:20:47 +0300

    Main Page - add logic to home-slider

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ Gulp, Pug, Scss
 
 dev build:
 `npm run dev`
+
+TODO:
++ home: fix bug with btn
+- add hovers to all btns
+- add sorting for plants by price
+- add page after click 'Visit Shop'
+- add email modal ?
---
+++ src/assets/sections/home/home.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  const slidesList = document.querySelectorAll('.slider-item');
+  const btnRight = document.querySelector('.home-slider-button.right');
+  const btnLeft = document.querySelector('.home-slider-button.left');
+
+  const slidesCount = slidesList.length;
+  //const slideWidth = window.getComputedStyle(slidesList[0]).getPropertyValue('width');
+  let currSlide = 1;
+  slidesList[0].classList.add('active');
+
+  btnLeft.addEventListener('click', () => {
+    currSlide = (currSlide > 1) ? (currSlide - 1) : slidesCount;
+    switchSlide();
+  });
+
+  btnRight.addEventListener('click', () => {
+    currSlide = (currSlide < slidesCount) ? (currSlide + 1) : 1;
+    switchSlide();
+  });
+
+  function switchSlide() {
+    slidesList.forEach(slide => {
+      let slideToSwitch = parseInt(slide.getAttribute('data-slide'));
+      if (currSlide === slideToSwitch) {
+        slide.classList.add('active');
+      } else {
+        slide.classList.remove('active');
+      }
+    })
+  }
+
+  console.log(slidesList[0])
+});
